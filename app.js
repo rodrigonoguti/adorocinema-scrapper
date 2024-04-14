e74355e15e82492babe5dbb70ed620aee920851b
Rodrigo Noguti <rodrigonoguti@gmail.com>
2024-04-14 13:45:40 -0300

    add fastify/cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const path = require('node:path')
 const AutoLoad = require('@fastify/autoload')
+const cors = require('@fastify/cors')
 
 // Pass --options via CLI arguments in command to enable these options.
 const options = {}
@@ -24,6 +25,10 @@ module.exports = async function (fastify, opts) {
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
     options: Object.assign({}, opts)
+  })
+
+  await fastify.register(cors, { 
+    origin: true
   })
 }
 
